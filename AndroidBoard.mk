416d2ddfb649f02fd64c7deac5f96a35d517cc5c
Matthew <m.hanhimaki@gmail.com>
2014-10-22 15:36:10 -0400

    adding inline kernel build

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -13,6 +13,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-TARGET_PREBUILT_KERNEL := device/lge/geeb-kernel/kernel
-endif
+
+KERNEL_DEFCONFIG := geeb_defconfig
+KERNEL_DIR := kernel
+
+-include $(TOP)/$(KERNEL_DIR)/AndroidKernel.mk
+
+# device.mk doesn't know about us, and we can't PRODUCT_COPY_FILES here.
+# So cp will do.
+.PHONY: $(PRODUCT_OUT)/kernel
+$(PRODUCT_OUT)/kernel: $(TARGET_PREBUILT_KERNEL)
+	cp $(TARGET_PREBUILT_KERNEL) $(PRODUCT_OUT)/kernel
